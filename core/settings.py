0eb6f67157991823cdf11b5d326a750358293997
shahbazbacker <shabzcc@gmail.com>
2020-11-25 19:52:29 +0530

    commented s3 static_url in settings

--- core/settings.py
+++ core/settings.py
@@ -196,7 +196,7 @@ AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = None
 
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage' 
 
