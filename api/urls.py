2086b103b972b2628525106b9e92cfaa2e4e3496
shahbazbacker <shabzcc@gmail.com>
2020-12-30 02:11:11 +0530

    fcm urls made as previous

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include, re_path
-from rest_framework import routers
-router = routers.DefaultRouter()
 from .views import *
 
 from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
 from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet
 router.register(r'devices', FCMDeviceAuthorizedViewSet)
 
-
+from rest_framework import routers
+router = routers.DefaultRouter()
 
 
 urlpatterns = [
     path('auth/', obtain_auth_token, name='api_token_auth'),
-    re_path(r'^devices?$', FCMDeviceAuthorizedViewSet.as_view({'post': 'create'}), name='create_fcm_device'),
+    repath(r'^', include(router.urls)),
     path('subjects/', SubjectView.as_view(), name="subjects"),
     path('chapters/<slug:slug>', ChapterView.as_view(), name="chapters"),
     path('documents/<slug:slug>', DocumentView.as_view(), name="documents"),
