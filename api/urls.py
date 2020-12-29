7b8426bfcf7d2e8aed086adcc70adf002f5b8eda
shahbazbacker <shabzcc@gmail.com>
2020-12-30 02:04:56 +0530

    fcmdjango router removed and added single create url

--- api/urls.py
+++ api/urls.py
@@ -5,14 +5,14 @@ from .views import *
 
 from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
 from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet
-router.register(r'devices', FCMDeviceAuthorizedViewSet)
+
 
 
 
 
 urlpatterns = [
     path('auth/', obtain_auth_token, name='api_token_auth'),
-    re_path(r'^', include(router.urls)),
+    re_path(r'^devices?$', FCMDeviceAuthorizedViewSet.as_view({'post': 'create'}), name='create_fcm_device'),
     path('subjects/', SubjectView.as_view(), name="subjects"),
     path('chapters/<slug:slug>', ChapterView.as_view(), name="chapters"),
     path('documents/<slug:slug>', DocumentView.as_view(), name="documents"),
