2f3099ab61f606d33caa484225a5717ced592d00
shahbazbacker <shabzcc@gmail.com>
2021-01-03 19:32:01 +0530

    sdsd

--- core/settings.py
+++ core/settings.py
@@ -193,14 +193,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CACHES = {
             "default": {
                 "BACKEND": "django_redis.cache.RedisCache",
-                "LOCATION": "/var/run/redis/redis.sock",
+                "LOCATION": "redis://127.0.0.1:6379/1",
                 "OPTIONS": {
                     "CLIENT_CLASS": "django_redis.client.DefaultClient",
                 }
             },
             'select2': {
                 "BACKEND": "django_redis.cache.RedisCache",
-                "LOCATION": "/var/run/redis/redis.sock",
+                "LOCATION": "redis://127.0.0.1:6379/2",
                 "OPTIONS": {
                     "CLIENT_CLASS": "django_redis.client.DefaultClient",
                 }
