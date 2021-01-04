ad82afef59e89cf480325cc99275a40514bd6294
shahbazbacker <shabzcc@gmail.com>
2021-01-05 03:14:05 +0530

    cda

--- lms_app/views.py
+++ lms_app/views.py
@@ -217,14 +217,15 @@ def video_list(request):
     
 
 def file_upload(request):
-    form = VideoUpload()
-    if request.method == 'POST':
-        form = VideoUpload(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
+    form = VideoUpload(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
         return redirect('video')
+    else:
+		form = VideoUpload()
     context = {'form': form}
     return render(request, 'lms_app/video_up.html', context)
+    
 
 
 class upload_document(ListView):
