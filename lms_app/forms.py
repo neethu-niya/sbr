33832b3c97fb0c3c49df2e80968ba4428ffe1798
Shahbaz Backer <shabzcc@gmail.com>
2021-01-08 00:54:01 +0530

    dsfdsfs

--- lms_app/forms.py
+++ lms_app/forms.py
@@ -97,6 +97,8 @@ class TeacherRegForm(forms.ModelForm):
 
 class StudentRegister(forms.ModelForm):
     syllabus = forms.ModelChoiceField(queryset=Syllabus.objects.all(), widget=forms.Select(attrs={"class":"form-control",type: "select", id:"addPosition"}))
+    standard = forms.ModelChoiceField(queryset=Standard.objects.all(), widget=forms.Select(attrs={"class":"form-control",type: "select", id:"addPosition"}))
+    scheme = forms.ModelChoiceField(queryset=Scheme.objects.all(), widget=forms.Select(attrs={"class":"form-control",type: "select", id:"addPosition"}))
 
     class Meta:
         model = Student
