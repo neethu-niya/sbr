56a963f9bcadc49980311a03329badacb5772bf6
Shahbaz Backer <shabzcc@gmail.com>
2021-01-12 00:46:55 +0530

    dfdaf

--- lms_app/views.py
+++ lms_app/views.py
@@ -226,7 +226,7 @@ def file_upload(request):
         standard = upload_video.standard
         users = User.objects.filter(student__standard=standard)
         for user in users:
-            FCMDevice.objects.filter(user=user).send_message(title="Uploaded New Video", body=upload_video.name)
+            FCMDevice.objects.filter(user=user).send_message(title="Uploaded New Video", body=f"{upload_video.name} of {upload_video.chapter}.")
         upload_video.save()
         return redirect('video')
     context = {'form': form}
